ae6678cb2809ac294031f67d09a0f50901d5ce43
Mehul Chaudhary <mehulchaudhary330@gmail.com>
2023-01-07 01:35:37 +0530

    added header, siedbar and body

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { flexbox, Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils//constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems={"center"}
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        yop: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: flexbox, alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+};
+
+export default Navbar;
